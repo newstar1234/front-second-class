fc04a5ea2515589ecbfb9141db18886065f0aa7b
newstar <maori36@naver.com>
2023-07-12 17:22:39 +0900

    23.07.12.16:30

--- src/components/Top3ListItem/index.tsx
+++ src/components/Top3ListItem/index.tsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './style.css';
+import { top3ListItemMock } from 'src/mocks';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Top3ListItem() {
+
+  const { boardNumber, boardTitle, boardContent, boardImage } = top3ListItemMock;
+  const { writerProfileImage, writerNickName, writerDate } = top3ListItemMock;
+  const { likeCount, viewCount, commentCount } = top3ListItemMock;
+
+  const navigator = useNavigate();
+
+  const onClickHandler = () => {
+    navigator(`/board/detail/${boardNumber}`);
+  }
+
   return (
-    <div>Top3ListItem</div>
+    <div className='top3-list-item-box' style={{ backgroundImage: `url(${ boardImage })`}} onClick={onClickHandler} >
+      <div className='top3-list-item-container'>
+        <div className='top3-list-item-writer'>
+          <div className='top3-list-item-profile'>
+            <div className='top3-list-item-profile-image' style={{ backgroundImage: `url(${ writerProfileImage })` }}></div>
+          </div>
+          <div className='top3-list-item-writer-right'>
+            <div className='top3-list-item-writer-nickname'>
+              { writerNickName }
+            </div>
+            <div className='top3-list-item-write-date'>
+              { writerDate }
+            </div>
+          </div>
+        </div>
+        <div className='top3-list-item-title'>
+          { boardTitle }
+        </div>
+        <div className='top3-list-item-content'>
+          { boardContent }
+        </div>
+        <div className='top3-list-item-count'>
+          { `댓글 ${commentCount} · 좋아요 ${likeCount} · 조회수 ${viewCount}` }
+        </div>
+      </div>
+    </div>
   )
 }
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -5,7 +5,20 @@ export const boardListItemMock = {
     writerDate: '2023. 07. 12',
     boardTitle: '비가내리는 날에는',
     boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at aliquet tortor. Proin vitae lacinia nunc. Maecenas a tortor turpis. Praesent accumsan egestas finibus. Proin sagittis euismod sagittis. Fusce mauris ex, interdum et laoreet a, mollis eu odio. Integer vel nulla eu augue dapibus porta sodales ac sapien. Fusce iaculis, neque id bibendum placerat, quam nisl efficitur diam, vitae vehicula arcu odio sed mauris. Integer orci elit, rutrum nec tortor sed, ultrices pretium nulla. Sed suscipit tristique lacus, eu rhoncus dolor aliquam ac. Aliquam non lorem leo.',
-    boardImage: 'https://www.sciencetimes.co.kr/wp-content/uploads/2022/06/%EB%82%B4%EA%B0%80-%EC%B0%8D%EC%9D%80-%EA%B5%AC%EB%A6%84-480x360.jpg',
+    boardImage: 'https://mblogthumb-phinf.pstatic.net/MjAyMDAxMTJfMjUz/MDAxNTc4Nzk0OTU3MzU0.tNgyHCs2j_dI-q0ceFtuJwaVwqHBs9cFC6ghB5fuSocg.T3saRbF6GlBgTCtf4n5cU2sOGJZDBskprT05kNiNc9gg.JPEG.dream_things/1578794957986.jpg?type=w800',
+    commentCount: 10,
+    likeCount: 10,
+    viewCount: 10,
+    boardNumber: 1,
+}
+
+export const top3ListItemMock = {
+    writerProfileImage: 'https://thumb.mtstarnews.com/06/2023/06/2023062107375547872_1.jpg',
+    writerNickName: '비가너무싫음',
+    writerDate: '2023. 07. 12',
+    boardTitle: '비가내리는 날에는',
+    boardContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at aliquet tortor. Proin vitae lacinia nunc. Maecenas a tortor turpis. Praesent accumsan egestas finibus. Proin sagittis euismod sagittis. Fusce mauris ex, interdum et laoreet a, mollis eu odio. Integer vel nulla eu augue dapibus porta sodales ac sapien. Fusce iaculis, neque id bibendum placerat, quam nisl efficitur diam, vitae vehicula arcu odio sed mauris. Integer orci elit, rutrum nec tortor sed, ultrices pretium nulla. Sed suscipit tristique lacus, eu rhoncus dolor aliquam ac. Aliquam non lorem leo.',
+    boardImage: 'https://mblogthumb-phinf.pstatic.net/MjAyMDAxMTJfMjUz/MDAxNTc4Nzk0OTU3MzU0.tNgyHCs2j_dI-q0ceFtuJwaVwqHBs9cFC6ghB5fuSocg.T3saRbF6GlBgTCtf4n5cU2sOGJZDBskprT05kNiNc9gg.JPEG.dream_things/1578794957986.jpg?type=w800',
     commentCount: 10,
     likeCount: 10,
     viewCount: 10,
