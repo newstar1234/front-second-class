4f04290af76b5904c245ef5e7c81acd934439ad1
newstar <maori36@naver.com>
2023-07-13 15:21:38 +0900

    23.07.13.14:30

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './style.css';
 
+interface Props {
+  label: string;
+  type: string;
+  placeholder: string;
+  helper: string;
+  icon?: string;
+} // mocks 데이터 말고  Props로 
 
-export default function InputBox() {
+export default function InputBox( { label, type, placeholder, helper, icon } : Props) {
+
   return (
-    <div>InputBox</div>
+    <div className='input-box'>
+      <div className='input-box-label'>{ label }</div>
+      <div className='input-box-container input-box-container-error'>
+        <input className='input' type={ type } placeholder={ placeholder } />
+        <div className='input-box-icon' >
+          <div className='input-on-icon'></div>
+        </div>
+      </div>
+      <div className='input-box-helper'>{ helper }</div>
+    </div>
   )
 }
