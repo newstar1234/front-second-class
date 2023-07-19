5d81d36aa661767e67f2511f893b901d87029814
newstar <maori36@naver.com>
2023-07-19 15:21:32 +0900

    23.07.19.14:30

--- src/views/Search/index.tsx
+++ src/views/Search/index.tsx
@@ -13,7 +13,7 @@ export default function Search() {
   const { searchWord } = useParams();  // 검색어 '안녕' //! console로 확인하기!
   
   const [boardCount, setBoardCount] = useState<number>(0);  // 검색어 카운트 
-  const [currentPage, setCurrentPage] = useState<number>(1);  //현재페이지 
+  const [currentPage, setCurrentPage] = useState<number>(1);  //현재 뿌려주는 게시물의 페이지 // 1은 1~5 // 2는 5~10
   const [searchList, setSearchList] = useState<SearchListResponseDto[]>([]);
   const [pageBoardList, setPageBoardList] = useState<SearchListResponseDto[]>([]);  //  실제로 보여줘야하는 페이지
 
@@ -23,17 +23,33 @@ export default function Search() {
     navigator(`/search/${word}`);
   }
 
-  useEffect(() => {
-    setSearchList(searchBoardListMock);
-    
-    const lastIndex = searchBoardListMock.length > COUNT_BY_PAGE ? COUNT_BY_PAGE : searchBoardListMock.length; //유연하게 // COUNT_BY_PAGE = 5
-    const pageBoardList = searchBoardListMock.slice(0, lastIndex);  //자르는 메서드 slice
+  const onPreviousClickHandler = () => {
+    setCurrentPage(currentPage -1);
+  }
+
+  const onNextClickHandler = () => {
+    setCurrentPage(currentPage +1);
+  }
+
+  const getPageBoardList = () => {  // 깔끔하게 함수로 
+    const lastIndex = searchBoardListMock.length > COUNT_BY_PAGE  * currentPage ? COUNT_BY_PAGE * currentPage : searchBoardListMock.length; //유연하게 // COUNT_BY_PAGE = 5
+    const startIndex = COUNT_BY_PAGE * (currentPage -1);  // pageBoardList를 유연하게 //등차수열
+    const pageBoardList = searchBoardListMock.slice(startIndex, lastIndex);  //자르는 메서드 slice
     
     setPageBoardList(pageBoardList);  //5개 잘라서 넣기
+  }
 
+  useEffect(() => {
+    setSearchList(searchBoardListMock);
     setBoardCount((searchWord as string).length);
     setRelationList(relationWordListMock);
+    
+    getPageBoardList();
   }, [searchWord]);
+
+  useEffect(() => {
+    getPageBoardList();
+  }, [currentPage]);
 
   return (
     <div id='search-wrapper'>
@@ -55,7 +71,19 @@ export default function Search() {
           </div>
         </div>
       </div>
-      <div className='search-pagination'></div>
+      <div className='search-pagination'>
+        <div className='pagination-button' onClick={onPreviousClickHandler}>
+          <div className='pagination-left-icon'></div>
+          <div className='pagination-button-text'>이전</div>
+        </div>
+        <div className='pagination-text'></div>
+        <div></div>
+        <div className='pagination-text'></div>
+        <div className='pagination-button' onClick={onNextClickHandler}>
+          <div className='pagination-button-text'>다음</div>
+          <div className='pagination-right-icon'></div>
+        </div>
+      </div>
     </div>
   )
 }
