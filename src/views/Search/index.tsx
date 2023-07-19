60c2686809460a6bdcf490e6fcd23d926381cc77
newstar <maori36@naver.com>
2023-07-19 15:01:25 +0900

    23.07.19.14:30

--- src/views/Search/index.tsx
+++ src/views/Search/index.tsx
@@ -1,17 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import './style.css';
 import BoardListItem from 'src/components/BoardListItem';
-import { useParams } from 'react-router-dom';
-import { relationWordListMock } from 'src/mocks';
+import { useNavigate, useParams } from 'react-router-dom';
+import { relationWordListMock, searchBoardListMock } from 'src/mocks';
+import { SearchListResponseDto } from 'src/interfaces/response';
 
 export default function Search() {
 
-  const { searchWord } = useParams();  // 검색어 '안녕'
+  const navigator = useNavigate();
 
-  const [boardCount, setBoardCount] = useState<number>(0);  
+  const { searchWord } = useParams();  // 검색어 '안녕' //! console로 확인하기!
+  
+  const [boardCount, setBoardCount] = useState<number>(0);  // 검색어 카운트 
+  const [currentPage, setCurrentPage] = useState<number>(1);  //현재페이지 
+  const [searchList, setSearchList] = useState<SearchListResponseDto[]>([]);
+  const [pageBoardList, setPageBoardList] = useState<SearchListResponseDto[]>([]);  //  실제로 보여줘야하는 페이지
+
   const [relationList, setRelationList] = useState<string[]>([]);
 
+  const onRelationClickHandler = (word: string) => {
+    navigator(`/search/${word}`);
+  }
+
   useEffect(() => {
+    setSearchList(searchBoardListMock);
+    
+    const pageBoardList = searchBoardListMock.slice(0, 5);  //자르는 메서드 slice
+    // [];
+    // for (let index = 0; index < 5; index++) {
+    //   pageBoardList.push(searchBoardListMock[index]);
+    // }  //! 이건 좀 무식한 방법
+    setPageBoardList(pageBoardList);  //5개 잘라서 넣기
+
     setBoardCount((searchWord as string).length);
     setRelationList(relationWordListMock);
   }, [searchWord]);
@@ -25,13 +45,13 @@ export default function Search() {
       </div>
       <div className='search-container'>
         <div className='search-board-list'>
-          
+          { pageBoardList.map( (item) => (<BoardListItem item={item} />) ) }
         </div>
         <div className='search-relation-box'>
           <div className='search-relation-card'>
             <div className='search-relation-text'>관련 검색어</div>
             <div className='search-relation-list'>
-              {relationList.map( (item) => (<div className='relation-chip'>{item}</div>) )}
+              {relationList.map( (item) => (<div className='relation-chip' onClick={ () => onRelationClickHandler(item) } >{item}</div>) )}
             </div>
           </div>
         </div>
