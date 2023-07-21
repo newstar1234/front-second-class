50f920c6abe16981c349968aa9568d7760994b15
newstar <maori36@naver.com>
2023-07-21 18:15:30 +0900

    23.07.21.17:30

--- src/views/Board/Detail/index.tsx
+++ src/views/Board/Detail/index.tsx
@@ -19,16 +19,49 @@ export default function BoardDetail() {
       <div className='board-detail-container'>
         <div className='board-detail-top'>
           <div className='board-detail-title-container'>
-            <div className='board-detail-title'></div>
+            <div className='board-detail-title'>오늘은 즐거운 금요일입니다.</div>
           </div>
           <div className='board-detail-meta-container'>
-            <div className='board-detail-meta-left'></div>
-            <div className='board-detail-meta-right'></div>
+            <div className='board-detail-meta-left'>
+              <div className='board-detail-writer-profile-image'></div>
+              <div className='board-detail-writer-nickname'>아이디</div>
+              <div className='board-detail-write-date'>{'\|'}</div>
+              <div className='board-detail-write-date'>2023. 07. 21.</div>
+            </div>
+            <div className='board-detail-meta-right'>
+              <div className='board-detail-more-button'>
+                <div className='more-icon'></div>
+              </div>
+            </div>
           </div>
         </div>
         <div className='divider'></div>
-        <div className='board-detail-middle'></div>
-        <div className='board-detail-bottom'></div>
+        <div className='board-detail-middle'>
+          <div className='board-detail-content'>퇴근하고싶어요.</div>
+          <div className='board-detail-image-box'>
+            <img className='board-detail-image' />
+          </div>
+        </div>
+        <div className='board-detail-bottom'>
+          <div className='board-detail-bottom-item'>
+            <div className='board-detail-bottom-button'>
+              <div className='favorite-icon'></div>
+            </div>
+            <div className='board-detail-bottom-text'>{`좋아요 0`}</div>
+            <div className='board-detail-bottom-button'>
+              <div className='down-icon'></div>
+            </div>
+          </div>
+          <div className='board-detail-bottom-item'>
+            <div className='board-detail-bottom-button'>
+              <div className='comment-icon'></div>
+            </div>
+            <div className='board-detail-bottom-text'>{`댓글 0`}</div>
+            <div className='board-detail-bottom-button'>
+              <div className='down-icon'></div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
