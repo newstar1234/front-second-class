b41edc4a3572d63605f1b47652264caebe997cfa
newstar <maori36@naver.com>
2023-07-21 15:31:18 +0900

    23.07.21.14:30

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -6,15 +6,15 @@ import { useLocation, useNavigate } from 'react-router-dom';
 export default function Header() {
 
   const [searchState, setSearchState] = useState<boolean>(false);
-  const [login, setLogin] = useState<boolean>(false); 
+  const [login, setLogin] = useState<boolean>(true);
 
   const navigator = useNavigate();
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
 
-  const showSearch = pathname !== '/my-page' && pathname !== '/board/write' && pathname.indexOf('board/update') === -1;
+  const showSearch = pathname !== '/my-page' && pathname !== '/board/write' && pathname.indexOf('/board/update') === -1;
   const isAuth = pathname === '/auth';
   const isMyPage = pathname === '/my-page';
-  const showUpload = pathname === '/board/wirte' || pathname.indexOf('/board/update') !== -1;
+  const showUpload = pathname === '/board/write' || pathname.indexOf('/board/update') !== -1;
 
   const onLogoClickHandler = () => {
     navigator('/');
@@ -22,7 +22,7 @@ export default function Header() {
 
   const onSignInButtonClickHandler = () => {
     setLogin(true);
-    navigator('/auth'); 
+    navigator('/auth');
   }
 
   const onMyPageButtonClickHandler = () => {
@@ -34,38 +34,31 @@ export default function Header() {
     navigator('/');
   }
 
-
   return (
     <div id='header'>
       <div className='header-left' onClick={onLogoClickHandler}>
         <div className='header-left-logo-icon'></div>
-        <div className='header-left-logo-text'>NEWSTAR's Board</div>
+        <div className='header-left-logo-text'>Hoons Board</div>
       </div>
       <div className='header-right'>
-        { ( showSearch ) && 
-        (searchState ? 
-          (
+        {(showSearch) && (searchState ? (
             <div className='header-search-box'>
-              <input className='header-search-input'/>
-              <div className='header-icon-box' onClick={ () => setSearchState(false) }>
+              <input className='header-search-input' />
+              <div className='header-icon-box' onClick={() => setSearchState(false)}>
                 <div className='header-search-icon'></div>
               </div>
             </div>
-          ) :
-          (
-            <div className='header-icon-box' onClick={ () => setSearchState(true) }>
-             <div className='header-search-icon'></div>
+          ) : (
+            <div className='header-icon-box' onClick={() => setSearchState(true)}>
+              <div className='header-search-icon'></div>
             </div>
-          ))
-          }
-        {
-          !isAuth && ( 
-            isMyPage ? (<div className='header-white-button' onClick={onSignOutButtonClickHandler}>로그아웃</div>) :
-            showUpload ? (<div className='header-black-disable-button'>업로드</div>) :
-            login ? (<div className='header-white-button' onClick={onMyPageButtonClickHandler}>마이페이지</div>) :
-            (<div className='header-black-button' onClick={onSignInButtonClickHandler}>로그인</div> ) 
-          )
-        }
+        ))}
+        {!isAuth && (
+          isMyPage ? (<div className='header-white-button' onClick={onSignOutButtonClickHandler}>로그아웃</div>) :
+          showUpload ? (<div className='header-black-disable-button'>업로드</div>) :
+          login ? (<div className='header-white-button' onClick={onMyPageButtonClickHandler}>마이페이지</div>) :
+                  (<div className='header-black-button' onClick={onSignInButtonClickHandler}>로그인</div>)
+        )}
       </div>
     </div>
   )
--- src/views/Board/Write/index.tsx
+++ src/views/Board/Write/index.tsx
@@ -2,6 +2,20 @@ import React from 'react'
 import './style.css';
 
 export default function BoardWrite() {
+
+  //              state             //
+
+  //              function             //
+
+  //              event handler             //
+
+  //              component             //
+  // description : 게시물 쓰기 화면 //
+
+  //              effect             //
+
+
+  //              render              //
   return (
     <div id='board-write-wrapper'>
       <div className='board-write-container'>
@@ -17,13 +31,12 @@ export default function BoardWrite() {
             <div className='image-upload-button'>
               <div className='image-upload-icon'></div>
             </div>
-            <input type='file' accept='image/*'/>
+            <input type='file' accept='image/*' style={{display: 'none'}}/>
           </div>
         </div>
         <div className='board-write-image-container'>
-          <div className='board-write-image'>
+          <img className='board-write-image' src='https://s3.ap-northeast-2.amazonaws.com/img.kormedi.com/news/article/__icsFiles/artimage/2018/06/29/c_km601/shutterstock_222269095_580.jpg' />
             <div className='board-write-image-delete-button'></div>
-          </div>
         </div>
       </div>
     </div>
